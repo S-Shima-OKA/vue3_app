b48097d836b0a2a477ac62d0c8e30f65e9ea2742
luv1024.read@gmail.com <luv1024.read@gmail.com>
2024-03-05 01:48:57 +0900

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import { VitePluginFonts } from 'vite-plugin-fonts'
+// import { VitePluginFonts } from 'vite-plugin-fonts'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    VitePluginFonts({
-      google: {
-        families: [
-          'Noto Sans JP' // ここに導入したいフォント名を入れる
-        ],
-      }
-    })
+    // VitePluginFonts({
+    //   google: {
+    //     families: [
+    //       'Noto Sans JP' // ここに導入したいフォント名を入れる
+    //     ],
+    //   }
+    // })
   ],
   resolve: {
     alias: {
